cfc3b389eae9b9ae3228376dc17dc061ad9dea15
Varazir <varazir@gmail.com>
2019-07-16 13:25:31 +0200

    not working

--- IKEARemote.lua
+++ IKEARemote.lua
@@ -1,134 +1,181 @@
 --
--- Creater Varazir ( And big helt from waaren and others on domotiz forum )
--- E-mail: varazir .. gmail.com
--- Version: 1.0
--- Forum https://www.domoticz.com/forum/viewtopic.php?f=59&t=28489
+-- Creater Varazir ( And big help from waaren and others on domotiz forum )
+-- e-mail: varazir .. gmail.com
+-- Version: 1.2
 --
-
-
-
-groups = {'group1', 'group2', 'group3', 'group4', 'group5', 'group6' }
-
+groups = {'group1', 'group2', 'group3', 'group4', 'group5', 'group6', 'group7' }
 return  {
-    on = {  devices = { 'IKEA Remote*' }},
+    on =        {
+                    devices         = { 'IKEA Remote*' }},
 
-    logging = { level = domoticz.LOG_DEBUG, 
-                marker = "IKEARemote" },
+    logging =   { 
+                    level           = domoticz.LOG_DEBUG, 
+                    marker          = "IKEARemote" 
+                },
 
-    data = { currentGroup = { initial = 1 }}, 
+    data =      { 
+                    currentGroup    = { initial = 1 }
+                }, 
 
     execute = function(dz, item)
         
         control = 
         {
-            group3 = --Hallway    
-            {
-                lamp =  { idx = 40, toggle = true, dimmer = true, blink = true }, 
-                --spot1 = { idx = 15, toggle = true, dimmer = true},
-				--spot2 = { idx = 17, toggle = true, dimmer = true},
-				--spot3 = { idx = 19, toggle = true, dimmer = true}
+            group1 =
+            { 
+                name            = 'Taklampa Sovrum',
+                IKEAlamp        = { idx = 13, toggle = true, blink = true, dimmer = true}
             },
+
+			group2 =
+            { 
+                name            = 'Rullgardin Sovrum',
+                Blinder         = { idx = 59, toggle = true, blinder = true }, 
             
-            group1 = --Bedroom
+            group3 =  
+            {
+                name            = 'Taklampa Hall',
+                IKEAlamp        = { idx = 40, blink = true }, 
+                IKEAlampGroup   = { idx = 65, toggle = true, dimmer = true},
+			},
+        
+			group4 =
             { 
-                lamp =  { idx = 13, toggle = true, blink = true, dimmer = true}
+                name            = 'Taklampa Datorrum',
+                IKEAlamp        =  { idx = 73, toggle = true, blink = true, dimmer = true}
             },
-            group4 = --Lilvingroom
+        
+            group5 =
             { 
-                --spot1 = { idx = 21, toggle = true, dimmer = true},
-                spot2 = { idx = 36, toggle = true, dimmer = true, blink = true} 
-                --spot3 = { idx = 25, toggle = true, dimmer = true}
+                name            = 'Taklampa Vardagsrum',
+                IKEAlampGroup   = { idx = 36, toggle = true, dimmer = true, blink = true} 
             },
-			group6 = --Computeroom
+
+			group6 = 
             { 
-                lamp =  { idx = 73, toggle = true, blink = true, dimmer = true}
+                name            = 'Tv Bänk',
+                Lamp            = { idx = 42, toggle = true, blink = true},
+                TvGroup         = { idx = 2,  toggle = true, group = true}
+            },
+        
+            },			
+			group7 =
+            { 
+                name            = 'Rullgardin Vardagsrum',
+                Blinder         = { idx = 83, toggle = true, blinder = true }, 
 
             },
-			group5 = --TvBench  
-            { 
-                lamp =  { idx = 42, toggle = true, blink = true},
-                onkyoHW = { idx = 74, toggle = true},
-                tv =    { idx = 45, toggle = true},
-				onkyo = { idx = 43, toggle = true}
-            },
---			group2 = --BedroomBlilnder
---            { 
---               blinder = { idx = 59, toggle = true, dimmer = true }, 
---
---            },
         }
-        local selectedGroupNumber = dz.data.currentGroup
-        local maxGroup = #groups
+
+        local function logWrite(str,level)
+            dz.log(tostring(str),level or dz.LOG_DEBUG)
+        end
         
+        local selectedGroupNumber = dz.data.currentGroup
+        logWrite("Current Group " .. selectedGroupNumber)
+        local maxGroup = #groups
+        local dummyDimmer = dz.devices(82)
+
         local function doAction(action, direction)
-            selectedGroup = groups[selectedGroupNumber]
-            dz.log("Current selected group is." .. selectedGroup)
-            dz.log("Current acction is........" .. action)
+            logWrite("Current group number......" .. selectedGroupNumber)
+            selectedGroup = 'group' .. selectedGroupNumber
+            logWrite("Current selected group is." .. selectedGroup)
+            logWrite("Current acction is........" .. action)
+            logWrite(control[selectedGroup])
             for device, attributes in pairs(control[selectedGroup]) do
-                dz.log("Current typ is............" .. device)
-                currentIDx = attributes["idx"]
-                currentDevice = dz.devices(currentIDx)
-                dz.log("Current device is........." .. currentDevice.name)
-                dz.log("Current device IDx is....." .. currentIDx)
-                for attribute, value in pairs(attributes) do
-                    dz.log("Current attribute is......" .. attribute)
-                    if attribute == action then
-                        dz.log("Current acction is " .. action)
-                        -- Blinking 
-                        if action == 'blink' then
-							local blinkDevice = dz.devices(currentDevice.name)
-							local blinkLevel = currentDevice.level
-							dz.log("Device " .. blinkDevice.name .. " will blink")
-							if blinkDevice.state == "Off" then 
-								blinkDevice.switchOn()
-								blinkDevice.switchOff().afterSec(0.5)
-							else
-								blinkDevice.switchOff()
-								blinkDevice.switchOn().afterSec(0.5)
-							end
-				        elseif action == 'dimmer' then 
-							local dimDevice = dz.devices(currentDevice.name)
-							local dimLevel = dimDevice.level
-							local delay = 0
-                            dz.log(dimDevice.name .. " direction is " .. direction)
-							if direction == "stop" then 
-							    dimDevice.cancelQueuedCommands()
-							    dz.log('Stop dimming of ' .. dimDevice.name .. ' at ' .. dimLevel ..'%')
-							elseif direction == 'down' then
-								repeat
-									delay = delay + 0.1
-									if direction == "down" then dimLevel = dimLevel - 1	else dimLevel = dimLevel + 1 end
-									dz.log('Set ' .. dimDevice.name .. ' to dimLevel '.. dimLevel .. '%, after ' .. delay .. ' seconds', dz.LOG_INFO)
-									dimDevice.dimTo(dimLevel).afterSec(delay)
-								until dimLevel <= 0
-						    elseif direction == 'up' then
-                                repeat
-                                    delay = delay + 0.1
-                                    dimLevel = dimLevel + 1
-                                    dz.log('Set ' .. dimDevice.name .. ' to dimLevel '.. dimLevel .. '%, after ' .. delay .. ' seconds', dz.LOG_INFO)
-                                    dimDevice.dimTo(dimLevel).afterSec(delay)
-                                until dimLevel >= 100
-							end
-                        elseif action == 'toggle' then
-                            local toggleDevice = dz.devices(currentDevice.name)
-                            local toggleState = toggleDevice.state
-                            if toggleState  == 'On' then toggleDevice.switchOff() else toggleDevice.switchOn() end
+                logWrite("Current device is........." .. device)
+                if device == "name" then
+                    logWrite('Name:.....................'  .. attributes)
+                    selectedGroupName = attributes
+                    dz.devices('IKEA Remote Groups').switchSelector(attributes).silent()
+                    -- dz.notify("Aktuell grupp",attributes,dz.PRIORITY_NORMAL,dz.NSS_HTTP)
+                else
+                    currentIDx = selectedGroup["idx"]
+                    logWrite("Current device IDx is....." .. currentIDx)
+                    if attributes["group"] then currentDevice = dz.groups(currentIDx) else  currentDevice = dz.devices(currentIDx) end
+                    logWrite("Current device name is........." .. currentDevice.name)
+                    for attribute, value in pairs(attributes) do
+                        logWrite("Current attribute is......" .. attribute)
+                        if attribute == action then
+                            logWrite("Current acction is " .. action)
+                            -- Blinking 
+                            if action == 'blink' then
+    							local blinkDevice = dz.devices(currentDevice.name)
+    							local blinkLevel = currentDevice.level
+    							logWrite("Device " .. blinkDevice.name .. " will blink")
+    							--if blinkDevice.state == "Off" then 
+    							--	blinkDevice.switchOn()
+    							--	blinkDevice.switchOff().afterSec(0.5)
+    							--else
+    							--	blinkDevice.switchOff()
+    							--	blinkDevice.switchOn().afterSec(0.5)
+    							--end
+    							
+    				        elseif action == 'dimmer' then 
+    							local dimDevice = dz.devices(currentDevice.name)
+    							local dimLevel = dimDevice.level
+    							local delay = 0
+                                logWrite(dimDevice.name .. " direction is " .. direction)
+    							if direction == "stop" then 
+    							    dimDevice.cancelQueuedCommands()
+    							    logWrite('Stop dimming of ' .. dimDevice.name .. ' at ' .. dimLevel ..'%')
+    							elseif direction == 'down' then
+    								repeat
+    									delay = delay + 0.1
+    									if direction == "down" then dimLevel = dimLevel - 1	else dimLevel = dimLevel + 1 end
+    									logWrite('Set ' .. dimDevice.name .. ' to dimLevel '.. dimLevel .. '%, after ' .. delay .. ' seconds')
+    									dimDevice.dimTo(dimLevel).afterSec(delay)
+    								until dimLevel <= 0
+    						    elseif direction == 'up' then
+                                    repeat
+                                        delay = delay + 0.1
+                                        dimLevel = dimLevel + 1
+                                        logWrite('Set ' .. dimDevice.name .. ' to dimLevel '.. dimLevel .. '%, after ' .. delay .. ' seconds')
+                                        dimDevice.dimTo(dimLevel).afterSec(delay)
+                                    until dimLevel >= 100
+    							end
+                            elseif action == 'toggle' then
+                                local toggleDevice = currentDevice
+                                if attributes["group"] then
+                                    toggleDevice.toggleGroup()
+                                else 
+                                    toggleDevice.toggleSwitch()
+                                end
+                            end
                         end
-                        
                     end
                 end
             end
         end
-
+        
+        
+        local function findGroup(groupname)
+            local GroupNumber = 0
+            repeat 
+                GroupNumber = GroupNumber + 1
+                local Group = control[GroupNumber]
+                for device, attributes in pairs(control[Group]) do
+                    if device == "name" then
+                        if attributes == groupname then
+                            selectedGroupNumber = GroupNumber
+                            return
+                        end
+                    end
+                end
+            until GroupNumber == maxGroup
+        end
+        
         local action = 'blink'
         local direction = 'up'
         
         if item.state == 'Click' and item.name == 'IKEA Remote Left' then 
             selectedGroupNumber = selectedGroupNumber - 1 
             if selectedGroupNumber == 0 then selectedGroupNumber = maxGroup end
+            -- dz.notify("Aktuell grupp",control[selectedGroupNumber],dz.PRIORITY_NORMAL,dz.NSS_HTTP)
         elseif item.state == 'Click' and item.name == 'IKEA Remote Right' then 
             selectedGroupNumber = selectedGroupNumber + 1 
             if selectedGroupNumber > maxGroup then selectedGroupNumber = 1 end
+            -- dz.notify("Aktuell grupp",control[selectedGroupNumber],dz.PRIORITY_NORMAL,dz.NSS_HTTP)
         elseif item.name == 'IKEA Remote' then
             action = 'toggle'
         elseif item.state == 'Hold' and item.name == "IKEA Remote Up"  then
@@ -139,16 +186,22 @@ return  {
         elseif item.state == 'Release' and item.name == 'IKEA Remote Down' or item.name == 'IKEA Remote Up' then
             action = 'dimmer'
             direction = 'stop'
+        elseif item.name == 'IKEA Remote Groups' then 
+            findGroup(item.levelName)
         else
-            dz.log('Unknown action requested; ignored' )
+            logWrite('Unknown action requested; ignored', dz.LOG_INFO )
             return
         end
-        doAction(action, direction) 
-        if item.levelName ==  'Click' then 
-            dz.log('Turning off' .. item.name)
+        
+        if item.state ==  'Click' then 
+            logWrite('Turning off ' .. item.name)
             dz.devices(item.name).switchOff().silent()
         end
+        
         dz.data.currentGroup = selectedGroupNumber
+        logWrite("Next Group " .. dz.data.currentGroup)
+        doAction(action, direction) 
+        
         
     end
 }
